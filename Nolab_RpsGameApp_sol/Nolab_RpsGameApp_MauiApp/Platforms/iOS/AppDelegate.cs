81b13685a6e8d23dd0c5d418b694b2e727763f4b
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-02-23 00:03:32 +0800

    init app
    新增遊戲圖片和主程式碼

---
+++ Nolab_RpsGameApp_sol/Nolab_RpsGameApp_MauiApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Nolab_RpsGameApp_MauiApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
